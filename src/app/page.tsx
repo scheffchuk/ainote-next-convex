b441840a365774aee8d6295418a5fd41ed0539e1
ScheffChuk <darthusian@gmail.com>
2025-06-25 11:05:34 +0900

    feat: Add lava lamp effect to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { LavaLamp } from "@/components/fluid-blob";
+
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center px-4 py-16">
-      <main className="max-w-4xl mx-auto text-center space-y-8">
+    <div className="relative min-h-screen flex flex-col items-center justify-center px-4 py-16">
+      <LavaLamp />
+      <main className="max-w-screen text-center space-y-8">
         {/* Header */}
-        <h1 className="text-4xl/relaxed font-bold text-gray-900">
+        <h1 className="text-4xl/relaxed font-bold tracking-tight mix-blend-exclusion text-white whitespace-nowrap">
           Building an AI-powered Note Taking App with Next.js and vercel AI SDK
         </h1>
         {/* LOGO */}
